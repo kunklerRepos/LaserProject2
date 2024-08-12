827faf873f3c1c34995dbf6d0c788e1a24738b92
Skunkler <42595638+Skunkler@users.noreply.github.com>
2024-08-12 01:03:00 -0400

    selected materials

--- laserproject.client/src/components/SelectMaterials.jsx
+++ laserproject.client/src/components/SelectMaterials.jsx
@@ -1,41 +1,90 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function SelectMaterial()
 {
     const navigate = useNavigate();
 
     const backBtnClickHandler = () => { navigate("/JobMenu"); }
-    const saveBtnClickHandler = () => {navigate("/JobMenu")}
+    const saveBtnClickHandler = () => { navigate("/JobMenu") }
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+
+    const handleSave = () => {
+        let selectedVal = document.getElementById("MaterialSelect");
+
+        let targetValues = MaterialObject[selectedVal.value];
+        
+        addItem(targetValues, selectedVal.value);
+        console.log(material);
+        handleClose();
+    }
+
+    const handleShow = () => setShow(true);
+
+    const MaterialObject = {
+        "Material #1": ["7.50", "10.10", "0.15"],
+        "Material #2": ["5.50", "8.50", "0.06"],
+        "Material #3": ["3.50", "5.50", "0.14"]
+
+    };
+
+    const SelectedMaterialObject = {};
+
+    const [material, setMaterial] = useState(SelectedMaterialObject);
+
+
+    const addItem = (value, key) => {
+        setMaterial(material => {
+            let copy = Object.assign({}, material)
+            copy[key] = value;
+            return copy ;
+        })
+    }
+
+    const removeItem = (value, e) => {
+        console.log(value);
+        let copiedMaterials = { ...material };
+        delete copiedMaterials[value];
+        setMaterial(material => ({
+            ...copiedMaterials
+
+        }));
+    }
+
+
+
     return (
         <>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="justify-content-center d-flex">
-                                <select id="MaterialSelect" style={{ marginBottom: "5%", width: "75%" }} className="form-control"></select>
-                            </div>
-
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="justify-content-center d-flex">
+                    <select id="MaterialSelect" style={{ marginBottom: "5%", width: "75%" }} className="form-control">
+                        <option value="Material #1">Material 1</option>
+                        <option value="Material #2">Material 2</option>
+                        <option value="Material #3">Material 3</option>
+                    </select>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleSave}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
             <h2>Select Material</h2>
             <br></br>
             <div className="justify-content-center" >
-                <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" className="btn btn-primary">Add Material</button>
+                <button variant="primary" onClick={handleShow} type="button" className="btn btn-primary">Add Material</button>
             </div> 
             <br></br>
                 <table className="table table-hover table-bordered">
@@ -44,9 +93,29 @@ function SelectMaterial()
                         <th scope="col">#</th>
                         <th scope="col" colSpan="2" >Material</th>
                         <th scope="col" >Unit Cost</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col" ></th>
                     </tr>
                 </thead>
                 <tbody>
+                    {Object.keys(material).map((item, index)=> {
+
+                        return (
+                            <Fragment>
+                                <tr key={index}>
+                                    <td  scope="row">{index + 1}</td>
+                                    <td  colSpan="2">{item}</td>
+                                    <td >{`$${material[item][0]}`}</td>
+                                    <td><input className="form-control" type="number"></input></td>
+                                    <td><a onClick={(e) => removeItem(item, e)}><i className="fa fa-trash-o"></i></a></td>
+                                </tr>
+
+                            </Fragment>
+                        )
+
+
+                    }) }
+
                 </tbody>
             </table>
             <button onClick={backBtnClickHandler} style={{ marginRight: "2%", backgroundColod: "white", color: "#1b6ec2", border: "1px solid #1b6ec2" }} type="button" className="btn">Cancel</button>
